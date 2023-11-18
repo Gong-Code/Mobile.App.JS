61866ef08325c8bb01f624b070c86743a47adb5a
Gong Moonphruk <gong.moonphruk@gmail.com>
2023-11-18 20:24:25 +0100

    setting up firebase DB

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-16fa1-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+
+
+
+
 const inputField = document.getElementById('input-field');
 const addButton = document.getElementById('add-button');
 
