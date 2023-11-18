78922955de82759a3593bc74e7515176917c9be3
Gong Moonphruk <gong.moonphruk@gmail.com>
2023-11-18 20:46:47 +0100

    Firebase DB is working

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://playground-16fa1-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "https://realtime-database-d7829-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
-
-
+const shoppingListInDB = ref(database, "shoppingList");
 
 
 const inputField = document.getElementById('input-field');
@@ -16,7 +15,10 @@ const addButton = document.getElementById('add-button');
 
 
 addButton.addEventListener("click", () => {
-    let inputValue = inputField.value;  
+    let inputValue = inputField.value; 
+    
+    push(shoppingListInDB, inputValue);
+
     console.log(inputValue)
 });
 
